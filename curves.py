9d25908ab9b267f97d38bb64d1935be5db7c799c
Hakob <tam.hak27@gmail.com>
2019-04-15 00:43:26 +0400

    add lstm

--- curves.py
+++ curves.py
@@ -1,9 +1,13 @@
 import numpy as np
 import math
+import warnings
+import itertools
+import numbers
 import torch
 import torch.nn.functional as F
 from torch.nn import Module, Parameter
 from torch.nn.modules.utils import _pair
+from torch.nn.modules.utils.rnn import PackedSequence
 from scipy.special import binom
 
 
@@ -252,6 +256,246 @@ class BatchNorm2d(_BatchNorm):
                              .format(input.dim()))
 
 
+_VF = torch._C._VariableFunctions
+_rnn_impls = {
+    'LSTM': _VF.lstm,
+    'GRU': _VF.gru,
+    'RNN_TANH': _VF.rnn_tanh,
+    'RNN_RELU': _VF.rnn_relu,
+}
+
+
+class LSTM(CurveModule):
+
+    def __init__(self, input_size, hidden_size, fix_points,
+                 num_layers=1, bias=True, batch_first=False,
+                 dropout=0, bidirectional=False):
+
+        num_directions = 2 if bidirectional else 1
+        all_param_names = []
+        for layer in range(num_layers):
+            for direction in range(num_directions):
+                suffix = '_reverse' if direction == 1 else ''
+                param_names = ['weight_ih_l{}{}', 'weight_hh_l{}{}']
+                if bias:
+                    param_names += ['bias_ih_l{}{}', 'bias_hh_l{}{}']
+                param_names = [x.format(layer, suffix) for x in param_names]
+                all_param_names += param_names
+        super(LSTM, self).__init__(fix_points, tuple(all_param_names))
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bias = bias
+        self.batch_first = batch_first
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+
+        if not isinstance(dropout, numbers.Number) or not 0 <= dropout <= 1 or \
+                isinstance(dropout, bool):
+            raise ValueError("dropout should be a number in range [0, 1] "
+                             "representing the probability of an element being "
+                             "zeroed")
+        if dropout > 0 and num_layers == 1:
+            warnings.warn("dropout option adds dropout after all but last "
+                          "recurrent layer, so non-zero dropout expects "
+                          "num_layers greater than 1, but got dropout={} and "
+                          "num_layers={}".format(dropout, num_layers))
+
+        gate_size = 4 * hidden_size
+        self._all_weights = []
+        for layer in range(num_layers):
+            for direction in range(num_directions):
+                layer_input_size = input_size if layer == 0 else hidden_size * num_directions
+
+                suffix = '_reverse' if direction == 1 else ''
+                param_names = []
+
+                for i, fixed in enumerate(self.fix_points):
+                    self.register_parameter(
+                        'weight_ih_l{}{}_{}'.format(layer, suffix, i),
+                        Parameter(torch.Tensor(gate_size, layer_input_size), requires_grad=not fixed)
+                    )
+                    param_names += ['weight_ih_l{}{}_{}'.format(layer, suffix, i)]
+                for i, fixed in enumerate(self.fix_points):
+                    self.register_parameter(
+                        'weight_hh_l{}{}_{}'.format(layer, suffix, i),
+                        Parameter(torch.Tensor(gate_size, hidden_size), requires_grad=not fixed)
+                    )
+                    param_names += ['weight_hh_l{}{}_{}'.format(layer, suffix, i)]
+                if bias:
+                    for i, fixed in enumerate(self.fix_points):
+                        self.register_parameter(
+                            'bias_ih_l{}{}_{}'.format(layer, suffix, i),
+                            Parameter(torch.Tensor(gate_size), requires_grad=not fixed)
+                        )
+                        param_names += ['bias_ih_l{}{}_{}'.format(layer, suffix, i)]
+                    for i, fixed in enumerate(self.fix_points):
+                        self.register_parameter(
+                            'bias_hh_l{}{}_{}'.format(layer, suffix, i),
+                            Parameter(torch.Tensor(gate_size), requires_grad=not fixed)
+                        )
+                        param_names += ['bias_hh_l{}{}_{}'.format(layer, suffix, i)]
+                    # else:
+                    #     self.register_parameter('bias_ih_l{}{}_{}'.format(layer, suffix, i), None)
+                    #     self.register_parameter('bias_hh_l{}{}_{}'.format(layer, suffix, i), None)
+
+                self._all_weights.append(param_names)
+
+        self.flatten_parameters()
+        self.reset_parameters()
+
+    def flatten_parameters(self):
+        """Resets parameter data pointer so that they can use faster code paths.
+
+        Right now, this works only if the module is on the GPU and cuDNN is enabled.
+        Otherwise, it's a no-op.
+        """
+        any_param = next(self.parameters()).data
+        if not any_param.is_cuda or not torch.backends.cudnn.is_acceptable(any_param):
+            return
+
+        # If any parameters alias, we fall back to the slower, copying code path. This is
+        # a sufficient check, because overlapping parameter buffers that don't completely
+        # alias would break the assumptions of the uniqueness check in
+        # Module.named_parameters().
+        all_weights = self._flat_weights
+        unique_data_ptrs = set(p.data_ptr() for p in all_weights)
+        if len(unique_data_ptrs) != len(all_weights):
+            return
+
+        with torch.cuda.device_of(any_param):
+            import torch.backends.cudnn.rnn as rnn
+
+            # NB: This is a temporary hack while we still don't have Tensor
+            # bindings for ATen functions
+            with torch.no_grad():
+                # NB: this is an INPLACE function on all_weights, that's why the
+                # no_grad() is necessary.
+                torch._cudnn_rnn_flatten_weight(
+                    all_weights, (4 * self.num_bends if self.bias else 2 * self.num_bends),
+                    self.input_size, rnn.get_cudnn_mode('LSTM'), self.hidden_size, self.num_layers,
+                    self.batch_first, bool(self.bidirectional))
+
+    def _apply(self, fn):
+        ret = super(LSTM, self)._apply(fn)
+        self.flatten_parameters()
+        return ret
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def check_forward_args(self, input, hidden, batch_sizes):
+        is_input_packed = batch_sizes is not None
+        expected_input_dim = 2 if is_input_packed else 3
+        if input.dim() != expected_input_dim:
+            raise RuntimeError(
+                'input must have {} dimensions, got {}'.format(
+                    expected_input_dim, input.dim()))
+        if self.input_size != input.size(-1):
+            raise RuntimeError(
+                'input.size(-1) must be equal to input_size. Expected {}, got {}'.format(
+                    self.input_size, input.size(-1)))
+
+        if is_input_packed:
+            mini_batch = int(batch_sizes[0])
+        else:
+            mini_batch = input.size(0) if self.batch_first else input.size(1)
+
+        num_directions = 2 if self.bidirectional else 1
+        expected_hidden_size = (self.num_layers * num_directions,
+                                mini_batch, self.hidden_size)
+
+        def check_hidden_size(hx, expected_hidden_size, msg='Expected hidden size {}, got {}'):
+            if tuple(hx.size()) != expected_hidden_size:
+                raise RuntimeError(msg.format(expected_hidden_size, tuple(hx.size())))
+
+        check_hidden_size(hidden[0], expected_hidden_size,
+                          'Expected hidden[0] size {}, got {}')
+        check_hidden_size(hidden[1], expected_hidden_size,
+                          'Expected hidden[1] size {}, got {}')
+
+    def forward(self, input, coeffs_t, hx=None):
+        is_packed = isinstance(input, PackedSequence)
+        if is_packed:
+            input, batch_sizes = input
+            max_batch_size = int(batch_sizes[0])
+        else:
+            batch_sizes = None
+            max_batch_size = input.size(0) if self.batch_first else input.size(1)
+
+        if hx is None:
+            num_directions = 2 if self.bidirectional else 1
+            hx = input.new_zeros(self.num_layers * num_directions,
+                                 max_batch_size, self.hidden_size,
+                                 requires_grad=False)
+            hx = (hx, hx)
+
+        self.check_forward_args(input, hx, batch_sizes)
+        _impl = _rnn_impls['LSTM']
+        if batch_sizes is None:
+            result = _impl(input, hx, self.compute_weights_t(coeffs_t), self.bias, self.num_layers,
+                           self.dropout, self.training, self.bidirectional, self.batch_first)
+        else:
+            result = _impl(input, batch_sizes, hx, self.compute_weights_t(coeffs_t), self.bias,
+                           self.num_layers, self.dropout, self.training, self.bidirectional)
+        output = result[0]
+        hidden = result[1:]
+
+        if is_packed:
+            output = PackedSequence(output, batch_sizes)
+        return output, hidden
+
+    def extra_repr(self):
+        s = '{input_size}, {hidden_size}'
+        if self.num_layers != 1:
+            s += ', num_layers={num_layers}'
+        if self.bias is not True:
+            s += ', bias={bias}'
+        if self.batch_first is not False:
+            s += ', batch_first={batch_first}'
+        if self.dropout != 0:
+            s += ', dropout={dropout}'
+        if self.bidirectional is not False:
+            s += ', bidirectional={bidirectional}'
+        return s.format(**self.__dict__)
+
+    def __setstate__(self, d):
+        super(LSTM, self).__setstate__(d)
+        if 'all_weights' in d:
+            self._all_weights = d['all_weights']
+        if isinstance(self._all_weights[0][0], str):
+            return
+        num_layers = self.num_layers
+        num_directions = 2 if self.bidirectional else 1
+        self._all_weights = []
+        for layer in range(num_layers):
+            for direction in range(num_directions):
+                suffix = '_reverse' if direction == 1 else ''
+                param_names = []
+
+                for i in range(len(self.fix_points)):
+                    param_names += ['weight_ih_l{}{}_{}'.format(layer, suffix, i)]
+                for i in range(len(self.fix_points)):
+                    param_names += ['weight_hh_l{}{}_{}'.format(layer, suffix, i)]
+                if bias:
+                    for i in range(len(self.fix_points)):
+                        param_names += ['bias_ih_l{}{}_{}'.format(layer, suffix, i)]
+                    for i in range(len(self.fix_points)):
+                        param_names += ['bias_hh_l{}{}_{}'.format(layer, suffix, i)]
+                self._all_weights.append(param_names)
+    @property
+    def _flat_weights(self):
+        return [p for layerparams in self.all_weights for p in layerparams]
+
+    @property
+    def all_weights(self):
+        return [[getattr(self, weight) for weight in weights] for weights in self._all_weights]
+
+
+
 class CurveNet(Module):
     def __init__(self, num_classes, curve, architecture, num_bends, fix_start=True, fix_end=True,
                  architecture_kwargs={}):
@@ -259,7 +503,7 @@ class CurveNet(Module):
         self.num_classes = num_classes
         self.num_bends = num_bends
         self.fix_points = [fix_start] + [False] * (self.num_bends - 2) + [fix_end]
-        
+
         self.curve = curve
         self.architecture = architecture
 
